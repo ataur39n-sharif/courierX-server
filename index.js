c6e8027773e18abab66a5743eca672f6925d681d
Ataur Rahman <rahman.sharif39n@gmail.com>
2021-04-30 15:38:10 +0600

    try 3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 app.use(express.json());
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
     next();
   });
 
